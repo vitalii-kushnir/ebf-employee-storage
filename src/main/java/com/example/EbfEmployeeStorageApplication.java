076b585204745b7eee124c7635847cefc00c9bb0
Vitalii Kushnir <kusha.apple@gmail.com>
2018-01-28 18:50:51 +0100

    Swagger UI was added

--- src/main/java/com/example/EbfEmployeeStorageApplication.java
+++ src/main/java/com/example/EbfEmployeeStorageApplication.java
@@ -2,11 +2,27 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class EbfEmployeeStorageApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EbfEmployeeStorageApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EbfEmployeeStorageApplication.class, args);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
